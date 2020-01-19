58ffff54387694f6059e73e6775cfc22a1c4e035
HILARY NG (INFRA-ISD-OOCLL/HKG) <NGHI2@oocl.com>
2020-01-19 19:57:27 +0800

    Finished

--- ddl.sql
+++ ddl.sql
@@ -1,31 +1,32 @@
 -- **Database level:**
 -- Display all the database
-
+show databases;
 -- Enter a certain database
-
+use <databaseName>;
 -- Create a database
-
+create database <databaseName>;
 -- Create the database of the designated character set
-
+create database <databaseName> character set <characterSet>;
 -- Display the creation information fo the database
-
+show create database <databaseName>;
 -- Revise the codes of the database
-
+alter database <DatabaseName> character set <CharacterSet>;
 -- Delete a database
-
+drop database <DatabaseName>;
 -- **Table level**
 -- Revise table name
-
+alter table <TableName> rename <NewTableName>;
 -- Revise the field's data type
-
+alter table <TableName> modify <FieldName> <DataType>
 -- Revise field name
-
+alter table <TableName> change <OldFieldName> <NewFieldName> <DataType>;
 -- Add field
-
+alter table <TableName> add <FieldName> <DataType>;
 -- Delete field
-
+alter table <TableName> drop <FieldName>
 -- Revise the table's storage engine
-
+alter table <TableName> engine=<StorageEngine>;
 -- Delete the table's foreign key restriant
-
+alter table <TableName> drop foreign key <ForeignKey>;
 -- Delete a table
+drop table <TableName>;
--- dml.sql
+++ dml.sql
@@ -1,7 +1,8 @@
 -- Insert record
-
+insert into <TableName> values(<Value1>,<Value2>...);
 -- Revise record
-
+update <TableName> set <Field>=<Value> where [CONDITION];
 --  Delete record
-
+delete from <TableName> where [CONDITION];
 -- Search record
+select <FieldName1>,<FieldName2> from <TableName> where [CONDITION];
